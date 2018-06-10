b0f5709569f8661f8433e99e78477c29289853a9
Meathill <meathill@gmail.com>
2018-06-10 23:53:24 +0800

    change: built the scaffold

---
+++ ui-src/gulpfile.babel.js
@@ -0,0 +1,33 @@
+import path from 'path';
+import gulp from 'gulp';
+import pug from 'gulp-pug';
+import stylus from 'gulp-stylus';
+import rename from 'gulp-rename';
+
+gulp.task('pug', () => {
+  return gulp.src('demo/*.pug')
+    .pipe(pug({
+
+    }))
+    .pipe(gulp.dest('html/'));
+});
+
+gulp.task('stylus', () => {
+  return gulp.src('stylus/screen.styl')
+    .pipe(stylus({
+      'include css': true,
+    }))
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('../'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('demo/**/*.pug', gulp.parallel('pug'));
+  gulp.watch('stylus/**/*.styl', gulp.parallel('stylus'));
+});
+
+gulp.task('default', gulp.parallel(
+  'pug',
+  'stylus',
+  taskDone => taskDone(),
+));
