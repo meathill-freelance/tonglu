f1e1affc25f677cbc4be29a24251feccda8eb97c
Meathill <meathill@gmail.com>
2018-06-19 01:38:10 +0800

    change: finish house

--- single-house.php
+++ single-house.php
@@ -14,10 +14,23 @@ if (have_posts()) {
   while (have_posts()) {
     the_post();
 
+    $slides = null;
     $content = get_the_content('继续阅读');
-    $value = get_post_meta($post->ID);
-    $value = strip_array_keys($value);
-    $blog = array(
+    $content = preg_replace_callback('/\[gallery[^\]]*ids="([\d,)]+)"[^\]]*\]/', function ($match) use (&$slides) {
+      $slides = explode(',', $match[1]);
+      $slides = array_map(function ($id, $index) {
+        return [
+          'key' => $index,
+          'url' => wp_get_attachment_url($id),
+          'is_first' => $index == 0,
+        ];
+      }, $slides, array_keys($slides));
+      return '';
+    }, $content);
+    $fields = get_post_meta($post->ID);
+    $fields = strip_array_keys($fields);
+    $fields['tags'] = explode(' ', $fields['tags']);
+    $blog = array_merge($fields, array(
       'id' => get_the_ID(),
       'is_featured' => is_sticky() && is_home() && ! is_paged(),
       'blog_class' => join(' ', get_post_class($class, $post_id)),
@@ -27,9 +40,11 @@ if (have_posts()) {
       'date' => apply_filters('the_time', get_the_time('Y-m-d'), 'Y-m-d'),
       'excerpt' => apply_filters('the_excerpt', get_the_excerpt()),
       'content' => apply_filters('the_content', $content),
-      'category' => get_the_category_list('<li class="breadcrumb-item">'),
+      'category' => get_the_category_list('</li><li class="breadcrumb-item">'),
       'thumbnail' => get_the_post_thumbnail_url(),
-    );
+      'slides' => $slides,
+      'theme_url' => get_theme_root_uri() . '/' . get_template(),
+    ));
   }
 }
 
